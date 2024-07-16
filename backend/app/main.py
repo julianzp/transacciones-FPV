40697926ff963efd5e57839dd2e72f3126d19ec8
julianzp <zapataparrajulian@gmail.com>
2024-07-15 20:20:16 -0500

    update CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -10,15 +10,9 @@ from pathlib import Path
 
 app = FastAPI()
 
-# Configurar CORS
-origins = [
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
-]
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
